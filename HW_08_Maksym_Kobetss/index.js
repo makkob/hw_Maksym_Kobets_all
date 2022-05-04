6a5b8bb879afb31fbee78ee6311b0f7c48dba997
makkob <maximkabig945@gmail.com>
2022-05-04 16:52:30 +0300

    hw8

--- HW_08_Maksym_Kobetss/index.js
+++ HW_08_Maksym_Kobetss/index.js
@@ -2,7 +2,7 @@ import gallery from "./gallery-items.js";
 
 let ulRef = document.querySelector(".js-gallery");
 
-gallery.forEach((img) => {
+gallery.forEach((img, index) => {
   let li = `
   <li class="gallery__item">
     <a
@@ -13,6 +13,7 @@ gallery.forEach((img) => {
         class="gallery__image"
         src="${img.preview}"
         data-source="${img.original}"
+        data-index="${index}"
         alt="${img.description}"
       />
     </a>
@@ -28,6 +29,7 @@ let lightBoxRef = document.querySelector(".lightbox");
 let lightBoxImageRef = document.querySelector(".lightbox__image");
 let lightBoxButtonRef = document.querySelector(".lightbox__button");
 let lightBoxOverlayRef = document.querySelector(".lightbox__overlay");
+let indexNow = 0;
 
 function open(event) {
   event.preventDefault();
@@ -40,21 +42,42 @@ function open(event) {
   lightBoxImageRef.alt = event.target.alt;
 
   lightBoxRef.classList.toggle("is-open");
-  //   idx = Number(event.target.dataset.index);
 
   lightBoxButtonRef.addEventListener("click", close);
   lightBoxOverlayRef.addEventListener("click", close);
 }
 
 function close() {
-  lightBoxRef.classList.toggle("is-open");
+  lightBoxRef.classList.remove("is-open");
   lightBoxImageRef.src = "";
-  //   window.removeEventListener("keydown", keyAction);
-  console.log("close");
 }
 
-// function next (){
+function nextImage() {
+  indexNow += 1;
+  let nextImg = document.querySelector(`[data-index="${indexNow}"]`);
+  lightBoxImageRef.src = nextImg.src;
+  lightBoxImageRef.alt = nextImg.alt;
+  lightBoxImageRef.dataset.idx = nextImg.idx;
+}
 
-// }
+function prevImage() {
+  indexNow -= 1;
+  let prevImg = document.querySelector(`[data-index="${indexNow}"]`);
+  lightBoxImageRef.src = prevImg.src;
+  lightBoxImageRef.alt = prevImg.alt;
+  lightBoxImageRef.dataset.idx = prevImg.idx;
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.code == "Escape") {
+    close();
+  }
+  if (event.code == "ArrowRight" && indexNow < gallery.length - 1) {
+    nextImage();
+  }
+  if (event.code == "ArrowLeft" && indexNow > 0) {
+    prevImage();
+  }
+});
 
 ulRef.addEventListener("click", open);
