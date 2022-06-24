296973caa985dce01202e5116934dbbc661ad7c2
makkob <maximkabig945@gmail.com>
2022-06-24 13:45:00 +0300

    hw11

--- HW_11_Maksym_Kobets/src/index.js
+++ HW_11_Maksym_Kobets/src/index.js
@@ -1,32 +1,43 @@
 import refs from "./js/refs";
 import "lodash";
 
-refs.input.addEventListener("input", _.debounce(getInput, 500));
-function getInput() {
-  let URI = `https://restcountries.com/v3.1/name/${refs.input.value}`;
-  refs.div.innerHTML = "";
-  fetch(URI)
-    .then((res) => res.json())
-    .then((data) => getData(data))
-    .catch(console.error);
-  function getData(data) {
-    if (data.length > 10) {
-      PNotify.alert({
-        title: "Уточните страну",
-      });
-    }
-    if (data.length <= 10 && data.length !== 1) {
-      data.map((country) => {
-        refs.div.insertAdjacentHTML(
-          "beforeend",
-          `<li> <img src="${country.flags.png}" style= "height:20px"> ${country.name.official} </li>`
-        );
-      });
-    }
-    if (data.length === 1) {
-      let templateScript = Handlebars.compile(refs.template);
-      let markup = templateScript(data);
-      refs.div.insertAdjacentHTML("beforeend", markup);
-    }
+class Counry {
+  constructor(refs) {
+    this.refs = refs;
   }
+  init = () => {
+    this.refs.input.addEventListener("input", _.debounce(this.getInput, 500));
+  };
+
+  getInput = () => {
+    let URI = `https://restcountries.com/v3.1/name/${this.refs.input.value}`;
+    this.refs.div.innerHTML = "";
+    fetch(URI)
+      .then((res) => res.json())
+      .then((data) => this.getData(data))
+      .catch(console.error);
+    this.getData = (data) => {
+      if (data.length > 10) {
+        PNotify.alert({
+          title: "Уточните страну",
+        });
+      }
+      if (data.length <= 10 && data.length !== 1) {
+        data.map((country) => {
+          this.refs.div.insertAdjacentHTML(
+            "beforeend",
+            `<li> <img src="${country.flags.png}" style= "height:20px"> ${country.name.official} </li>`
+          );
+        });
+      }
+      if (data.length === 1) {
+        let templateScript = Handlebars.compile(this.refs.template);
+        let markup = templateScript(data);
+        this.refs.div.insertAdjacentHTML("beforeend", markup);
+      }
+    };
+  };
 }
+let HWCountry = new Counry(refs);
+
+HWCountry.init();
